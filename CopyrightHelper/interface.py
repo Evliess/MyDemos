08a343335533165849e5f4aed44a432a2dfc3b14
Jia Jia Gui <shguijj@cn.ibm.com>
2020-11-22 16:57:21 +0800

    update ui

--- CopyrightHelper/interface.py
+++ CopyrightHelper/interface.py
@@ -3,38 +3,43 @@ from fileutil import read
 from fileutil import appendCopyrightHeader
 from fileutil import getSourceFiles
 
-supportedLanguages = ['Java', 'Javascript', 'Shell', 'Xml']
+supportedLanguages = ('Java', 'Javascript', 'Shell', 'Xml')
 
 def initUI():
+  background_color = '#1d3649'
+  button_backgroud_color = '#b4e051'
+  progress_bar_color = ['#b4e051', '#fff']
   layout = [
-    [sg.Text('Apply to', size=(8, 1)), sg.Input(size=(65, 1), key="source"), sg.FolderBrowse()],
-    [sg.Text('Formatting', size=(8, 1)), 
-    sg.Listbox(values=supportedLanguages, size=(63, 3), key='language', enable_events=True), 
-    sg.Button('Apply  ', key='apply')],
-    [sg.Text('Copyright Content', size=(60, 1))],
+    [sg.Text('Apply to', size=(8, 1), background_color=background_color), 
+    sg.Input(size=(65, 1), key="source"), 
+    sg.FolderBrowse(size=(6, 1), button_color=['#000', button_backgroud_color])],
+    [sg.Text('Formatting', size=(8, 1), background_color=background_color), 
+    sg.InputCombo(values=supportedLanguages, size=(63, 3), key='language', enable_events=True), 
+    sg.Button('Apply', size=(6, 1), key='apply', button_color=['#000', button_backgroud_color])],
+    [sg.Text('Copyright Content', size=(60, 1), background_color=background_color)],
     [sg.Multiline(size=(82, 15), key='content')],
-    [sg.Text('', size=(60, 2), key='message')],
-    [sg.ProgressBar(100, orientation='h', size=(44, 3), key='progressbar')]
+    [sg.Text('', size=(60, 2), key='message', background_color=background_color)],
+    [sg.ProgressBar(100, orientation='h', size=(46, 3), key='progressbar', bar_color=progress_bar_color)]
   ]
-  window = sg.Window('Copyright-Header Tool', layout, no_titlebar=False, finalize=True)
+  window = sg.Window('Copyright-Header Tool', layout, no_titlebar=False, finalize=True, background_color=background_color)
   handleWidgetEvents(window)
 
 
-def formattingListboxListener(window, values):
-    if values['language'][0] == "Java":
+def formattingComboxListener(window, values):
+    if values['language'] == "Java":
       window['content'].update(read('./copyrights/java-template'))
-    if values['language'][0] == "Javascript":
+    if values['language'] == "Javascript":
       window['content'].update(read('./copyrights/js-template'))
-    if values['language'][0] == "Shell":
+    if values['language'] == "Shell":
       window['content'].update(read('./copyrights/shell-template'))
-    if values['language'][0] == "Xml":
+    if values['language'] == "Xml":
       window['content'].update(read('./copyrights/xml-template'))
 
 def applyBtnListener(window, values):
   appendCopyrightHeader(window, getSourceFiles(values['source']), values['content'])
 
 def setDefaultLanguage(window):
-  window['language'].SetValue('Java')
+  window['language'].update('Java')
   window['content'].update(read('./copyrights/java-template'))
 
 def handleWidgetEvents(window):
@@ -44,7 +49,7 @@ def handleWidgetEvents(window):
     if event == 'apply':
       applyBtnListener(window, values)
     if event == 'language':
-      formattingListboxListener(window, values)
+      formattingComboxListener(window, values)
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
   window.close()
