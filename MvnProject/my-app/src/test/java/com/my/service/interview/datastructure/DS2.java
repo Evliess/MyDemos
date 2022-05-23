d4391c2a0366a87ae25548abdddf5a81ef4ae85d
jackgui <jiajia_yuwei@163.com>
2022-05-23 09:08:20 +0800

    dp1,2

--- MvnProject/my-app/src/test/java/com/my/service/interview/datastructure/DS2.java
+++ MvnProject/my-app/src/test/java/com/my/service/interview/datastructure/DS2.java
@@ -28,5 +28,44 @@ public class DS2 {
     System.out.println(maxSubArray(nums));
   }
 
+  @Test
+  public void fun1() {
+    int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int ans = Integer.MIN_VALUE;
+    int k = 2;
+    for (int i = 0; i < nums.length - k + 1; i++) {
+      int sum = 0;
+      for (int j = 0; j < k; j++) {
+        if (i + j < nums.length) {
+          sum += nums[i + j];
+        }
+      }
+      ans = sum > ans ? sum : ans;
+      System.out.println(sum);
+    }
+    System.out.println("ans: " + ans);
+  }
+
+  @Test
+  //timeout!!!
+  public void fun2() {
+    int[] nums = {5,4,-1,7,8};
+    int ans = Integer.MIN_VALUE;
+    for (int k = 1; k <= nums.length; k++) {
+      for (int i = 0; i < nums.length - k + 1; i++) {
+        int sum = 0;
+        for (int j = 0; j < k; j++) {
+          if (i + j < nums.length) {
+            sum += nums[i + j];
+          }
+        }
+        ans = sum > ans ? sum : ans;
+      }
+    }
+
+    System.out.println("ans: " + ans);
+
+  }
+
 
 }
